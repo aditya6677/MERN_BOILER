68416646b226f474da50d825c7fa5de0c96bcecc
Aditya Kumar <sankmr76@gmail.com>
2018-04-01 02:09:28 +0530

    mern boiler from keith

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://aditya76:sandy76#@localhost:27017/aditya',
+  db_dev: 'mongodb://localhost:27017/aditya',
+};
+
